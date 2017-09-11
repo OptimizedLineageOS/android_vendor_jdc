43f1e8bdc17abfaf264a54c7f26e711bd18c504d
dkati <dkatikaridis@gmail.com>
2017-09-11 19:23:38 +0300

    build.sh:Add CCACHE params
    just in case
    
    Change-Id: I21483fe8f9de31e52f2420331d97bd075198089c

--- build.sh
+++ build.sh
@@ -19,6 +19,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#export USE_CCACHE=1
+#export CCACHE_COMPRESS=1
 
 ROM_NAME="Palm Project"
 TARGET=Cheeseburger
