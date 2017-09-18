a9fb2becc07eacef31dcb65228225404245985fc
dkati <dkatikaridis@gmail.com>
2017-09-18 19:59:52 +0300

    emmc_trim:Turn LED on only if exists
    Change-Id: Ieb5e82428ec779e47b5b489da612e83af10b63e6

--- prebuilt/common/bin/emmc_trim.sh
+++ prebuilt/common/bin/emmc_trim.sh
@@ -6,13 +6,22 @@
 #
 # File system trim at each boot
 #
+
+if [ -e /sys/class/leds/blue/trigger ]
+then
+#Cheeseburger pattern
 LED=/sys/class/leds/blue/trigger
 echo heartbeat > $LED
+fi
 LOG=/cache/trim.log
 echo "*** $(date -u +%Y-%m-%d) ***" >> $LOG
 fstrim -v /cache >> $LOG
 fstrim -v /data >> $LOG
 fstrim -v /system >> $LOG
+if [ -e /sys/class/leds/blue/trigger ]
+then
+#Cheeseburger pattern
 echo none > $LED
+fi
 sync
 exit 0
